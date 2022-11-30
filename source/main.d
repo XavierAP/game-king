ba8af5b328245f77a972c8c9e9f6904ae9db4a62
XavierAP <n3minis-git@yahoo.es>
2022-11-30 21:45:13 +0100

    Rename and bugfix
    was bug when window was resized automatically (maximized to tablet mode)

--- source/main.d
+++ source/main.d
@@ -14,17 +14,17 @@ void main()
 	auto libSDL = loadLibSDL();
 	auto libSDLImage = loadLibSDLImage();
 
-	XY winSize = { 800, 600 };
-	auto mainWindow = createWindow(appTitle, SDL_WINDOW_RESIZABLE, winSize);
+	XY windowSize = { 800, 600 };
+	auto mainWindow = createWindow(appTitle, SDL_WINDOW_RESIZABLE, windowSize);
 	SDL_SetRenderDrawColor(mainWindow.render, 0x80, 0xA0, 0x60, 0xFF);
 
 	auto textures = loadTexture(dirImages~"People.png", mainWindow.render);
 	auto playerClip = clipTexture(textures, 2, 1);
 
+	SDL_GetWindowSize(mainWindow.window, &windowSize.x, &windowSize.y);
 	SDL_Rect rect = { w: mapTileSize, h: mapTileSize };
-	SDL_GetWindowSize(mainWindow.window, &rect.x, &rect.y);
-	rect.x = (rect.x - rect.w) / 2;
-	rect.y = (rect.y - rect.h) / 2;
+	rect.x = (windowSize.x - rect.w) / 2;
+	rect.y = (windowSize.y - rect.h) / 2;
 
 	// Main loop:
 	bool isRefreshNeeded = true;
@@ -61,9 +61,9 @@ void main()
 					// Re-center the image:
 					int w, h;
 					SDL_GetWindowSize(mainWindow.window, &w, &h);
-					rect.x += (w - winSize.x) / 2;
-					rect.y += (h - winSize.y) / 2;
-					winSize = XY(w, h);
+					rect.x += (w - windowSize.x) / 2;
+					rect.y += (h - windowSize.y) / 2;
+					windowSize = XY(w, h);
 					break;
 				
 				default: break;
